e38f22971aa1319e3b6e3c6ef2edd41f2343c74f
John Reading <john@resonancemultimedia.com>
2013-05-31 16:54:11 -0400

    sample modules

--- dist/widget.js
+++ dist/widget.js
@@ -23,7 +23,47 @@ define(['rclass'],function(rclass){
 			this.element = $(element);
 			// extend default options with options args, and data attr options
 			this.options = $.extend(this.options, options, this.element.data());
-			this.log('widget init', this.options, this.element);
+			this.log('component init', this.options, this.element);
+		},
+		/**
+		* publish
+		*
+		* Used to publish custom events.
+		*/
+		publish: function(ev) {
+			$(this.element).trigger(ev);
+		},
+		/**
+		* subscribe
+		*
+		* Used to subscribe to events from other modules.
+		*/
+		subscribe: function(ev, callback, element, args) {
+			$el = !element ? $('html') : $(element);
+			$el.unbind(ev).bind(ev, function(e){
+				callback(args);
+			});
+		},
+		/**
+		* AddAnimation
+		*
+		* Used to add animations css or jQuery fallback animation.
+		*/
+		toggleAnimation: function(css, callback, element) {
+
+			$el = !element ? this.element : $(element);
+
+			//$el.off(this.transitionEnd);
+			$el.on(this.transitionEnd, function() {
+				$el.off(this.transitionEnd);
+				if (callback) callback();
+			});
+			if (typeof css == "string") {
+				$el.hasClass(css) ? $el.removeClass(css) : $el.addClass(css);
+			} else {
+				$el.css(css);
+			}
+			/// TODO: break out class for jQuery animate
 		},
 		/**
 		* log
